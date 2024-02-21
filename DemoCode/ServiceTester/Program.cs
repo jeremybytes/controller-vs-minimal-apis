d35f8e9b80004133c3628b385fee7d8e90b34b1f
Jeremy Clark <jeremy@jeremybytes.com>
2024-02-21 12:10:06 -0800

    Experimenting with raw string responses

--- DemoCode/ServiceTester/Program.cs
+++ DemoCode/ServiceTester/Program.cs
@@ -4,24 +4,28 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        DataReader controllerReader = new("http://localhost:5062");
-        DataReader minimalReader = new("http://localhost:5194");
+        Uri controllerUri = new("http://localhost:5062");
+        Uri minimalUri = new("http://localhost:5194");
 
         // Minimal API Call
         Console.WriteLine("Minimal /people/v1/3");
-        var response = await minimalReader.SendRequest("/people/v1/3");
-        Console.WriteLine(response.data);
+        var response = await minimalUri.GetResponse("/people/v1/3");
+        Console.WriteLine(response);
 
         Console.WriteLine("----------");
 
         // Controller API Call
         Console.WriteLine("Controller /people/3");
-        response = await controllerReader.SendRequest("/people/3");
-        Console.WriteLine(response.data);
+        response = await controllerUri.GetResponse("/people/3");
+        Console.WriteLine(response);
 
         Console.WriteLine("----------");
 
         // Calls item that does not exist
+
+        //DataReader controllerReader = new("http://localhost:5062");
+        //DataReader minimalReader = new("http://localhost:5194");
+
         //Console.WriteLine("Controller /people/-1");
         //response = await controllerReader.SendRequest("/people/-1");
         //Console.WriteLine(response.status);
